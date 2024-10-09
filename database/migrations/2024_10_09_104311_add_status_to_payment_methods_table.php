69e7d3210a1191665a666d9a0a96d37a64e4afe8
Akhilkp27 <akhil499@gmail.com>
2024-10-09 14:50:24 +0530

    add fields for status and icon_url in payment method table

---
+++ database/migrations/2024_10_09_104311_add_status_to_payment_methods_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('payment_methods', function (Blueprint $table) {
+            $table->string('icon_url', 255)->nullable()->after('details');
+            $table->enum('status', ['active', 'inactive'])->default('active')->after('icon_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('payment_methods', function (Blueprint $table) {
+            $table->dropColumn('status');
+            $table->dropColumn('icon_url');
+        });
+    }
+};
